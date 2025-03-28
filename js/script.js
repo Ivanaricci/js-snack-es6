ef1589e80f2ca4b6f003a417d97dd10d02e6137e
Ivanaricci <ivana.ricci26@gmail.com>
2025-03-28 15:03:14 +0100

    [UPD] Create array snack1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 // Snack 1 Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare in console la bici con peso minore.
 
+const bike = [
+    {
+        nome: "Scott Addict RC Ultimate",
+        peso: 5.9 
+    },
+    {
+        nome: "Giant TCR Advanced SL 0",
+        peso: 6.4
+    },
+    {
+        nome: "Specialized S-Works Aethos",
+        peso: 6.34
+    }
+]
+
+console.log(bike)
