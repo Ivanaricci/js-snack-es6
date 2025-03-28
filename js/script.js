08e4886489e14d471e2205d41e6838c5f4513b60
Ivanaricci <ivana.ricci26@gmail.com>
2025-03-28 16:22:42 +0100

    [UPD] Add array squadre, punti e falli random

--- js/script.js
+++ js/script.js
@@ -30,3 +30,39 @@ for (let i=1; i<bike.length; i++){
 }
 
 console.log(`La bici con il peso minore è: ${pesoMin.nome}: ${pesoMin.peso} kg`)
+
+
+
+// Snack2 Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”. Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+
+const squadre = [
+    {
+        nome: "Milan",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Inter",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Juventus",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    }
+]
+
+const generateRandomNum = () => {
+    return Math.floor(Math.random() * 10) + 1
+}
+
+for (let i=0; i<squadre.length; i++){
+
+    squadre[i].puntiFatti = generateRandomNum();
+    squadre[i].falliSubiti = generateRandomNum();
+}
+
+console.log(squadre)
